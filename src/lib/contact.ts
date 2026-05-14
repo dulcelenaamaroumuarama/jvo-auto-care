cbc907aceada0e3ba2de64d96595868d4fc42e35
dulcelenaamaroumuarama <dulcelenaamaroumuarama@gmail.com>
2026-05-13 23:30:36 -0300

    Update contact.ts

--- src/lib/contact.ts
+++ src/lib/contact.ts
@@ -5,6 +5,7 @@ export const WHATSAPP_URL = `https://wa.me/${PHONE}?text=${encodeURIComponent(WH
 export const TEL_URL = `tel:+${PHONE}`;
 export const ADDRESS = "R. São Luíz, 3274 - Zona V, Umuarama - PR, 87504-220";
 export const HOURS = "Seg–Sex 08h–18h · Sáb 08h–12h";
-export const MAP_EMBED =
-  "https://www.google.com/maps?q=-23.753740,-53.316636&hl=pt-BR&z=17&output=embed";
-export const MAP_LINK = "https://www.google.com/maps?q=-23.753740,-53.316636";
+export const MMAP_EMBED =
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d270.73604177410795!2d-53.31677925499756!3d-23.75393589588761!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94f2d50042f68cf3%3A0x228ebef44cf8fa26!2sJVO%20AUTO-SERVI%C3%87OS!5e1!3m2!1spt-BR!2sbr!4v1778725049531!5m2!1spt-BR!2sbr";
+export const MAP_LINK =
+  "https://www.google.com/maps/place/JVO+AUTO-SERVI%C3%87OS/@-23.7539014,-53.3164988,21z/data=!4m16!1m9!3m8!1s0x94f2d50042f68cf3:0x228ebef44cf8fa26!2sJVO+AUTO-SERVI%C3%87OS!8m2!3d-23.7537895!4d-53.3166269!9m1!1b1!16s%2Fg%2F11zbcfbt_m!3m5!1s0x94f2d50042f68cf3:0x228ebef44cf8fa26!8m2!3d-23.7537895!4d-53.3166269!16s%2Fg%2F11zbcfbt_m?entry=ttu&g_ep=EgoyMDI2MDUxMS4wIKXMDSoASAFQAw%3D%3D";
